385f82e506700b49fae2fa10b5344f19a0798359
System Administrator <tysonhichman@gmail.com>
2024-06-20 13:45:01 -0500

    email hot fix

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -28,7 +28,7 @@ export default async function signup({
             email,
             password,
             options: {
-                emailRedirectTo: `${origin}`,
+                emailRedirectTo: window.location.origin,
             },
         });
 
